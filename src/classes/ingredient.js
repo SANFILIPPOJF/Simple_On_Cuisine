852eff930d1a519ef82b1a40671d865f12d0f535
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-12-12 16:18:23 +0100

    v0.12 classes

---
+++ src/classes/ingredient.js
@@ -0,0 +1,9 @@
+export class Ingredient{
+    constructor(){
+        this.id=id;
+        this.name=name;
+        this.description=description;
+        this.image=image;
+        this.imageSmall=imageSmall;
+    }
+}
