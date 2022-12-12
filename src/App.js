ac3cb32eeeb1053b89613deca1452e4a4b4b9c2d
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-12-12 15:00:20 +0100

    v0.1 readme old supprimé

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https:\/\/www.youtube.com\/watch?v=8U_29i9Qp5U"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
