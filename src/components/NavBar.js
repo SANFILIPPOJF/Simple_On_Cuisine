03f70ac0428771f55b977e9e696307ce0e85d7e4
Titou <menardjc.33@gmail.com>
2022-12-13 17:04:43 +0100

    feat navbar avec menu deroulant

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,23 +7,16 @@ export function CuisineNavBar() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
+                        <NavDropdown title="Search" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">By categories</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">By area</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">By ingredients</NavDropdown.Item>
                         </NavDropdown>
+                        <Nav.Link href="#link">Favoris</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
