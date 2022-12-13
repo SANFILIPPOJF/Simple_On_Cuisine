57f425e933ffc08af54b912fcf02959d0f8fdd65
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-12-13 13:44:53 +0100

    v0.2 plus maquette Jo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import { Navbar } from "./components/NavBar";
+import './maquette/maquette.css';
+import MAQUETTE from "./maquette/structure.json"
+import { InfoBox } from './maquette/infobox';
 
 function App() {
 
@@ -7,10 +10,10 @@ function App() {
     <div className='App'>
 
       <Navbar></Navbar>
-
+      <InfoBox data={MAQUETTE} />
     </div>
 
-);
+  );
 }
 
 export default App;
