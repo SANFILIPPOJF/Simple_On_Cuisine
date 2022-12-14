5a1cffcc7bbbef1fa798ec0d22dcf9732e555aec
Titou <menardjc.33@gmail.com>
2022-12-14 15:34:22 +0100

    feat: affichageRecette.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import { BarreDeRecherche } from './components/BarreDeRecherche';
 import { CuisineNavBar } from "./components/NavBar";
 import { Titre } from './components/Titre';
 import { BoutonRandom } from './components/BoutonRandom';
-import { Recette } from './components/affichageRecette';
 import { MealByRandom } from './requests/mealByRandom';
+import { AffichageRecette } from './components/affichageRecette';
 function App() {
   return (
     <div className='App'>
       <Titre></Titre>
       <BarreDeRecherche></BarreDeRecherche><BoutonRandom></BoutonRandom>
       <CuisineNavBar></CuisineNavBar>
-{/*       <Recette></Recette> */}
-      <MealByRandom></MealByRandom>
+{/*       <AffichageRecette></AffichageRecette> */}
+{/*       <MealByRandom></MealByRandom> */}
     </div>
 
   );
