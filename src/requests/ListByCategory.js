121b7ddbd81ed20ee3b81b421d63011271a7628b
GORON Delphine (SNCF VOYAGEURS / ESV TGV Sud Ouest / CAMPUS VOY TLSE BDX) <7803031f@commun.ad.sncf.fr>
2022-12-14 17:59:12 +0100

    création de la listByCategory

--- src/requests/ListByCategory.js
+++ src/requests/ListByCategory.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Accordion from 'react-bootstrap/Accordion';
+
 
 
 export function ListByCategory(props) {
@@ -8,23 +8,31 @@ export function ListByCategory(props) {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
-useEffect(() => {
+    useEffect(() => {
 
-    const category = { method: 'GET' };
-    fetch(`http://www.themealdb.com/api/json/v1/1/filter.php?c=${props.category}`)
-        .then(response => response.json())
-        .then((category) => {
-            console.log(category.meals[0].strMeal);
-            setLoading(false);
-            setData(category);
-        })
-        .catch((e) => {
-            console.error(`An error occurred: ${e}`)
-        });
-}, []);
-if (loading){
-    return (<p>Loading...</p>)
-}
+        {/*const category = { method: 'GET' };*/}
+        fetch(`http://www.themealdb.com/api/json/v1/1/filter.php?c=${props.category}`)
+            .then(response => response.json())
+            .then((category) => {
+                console.log(category.meals);
+                setLoading(false);
+                setData(category.meals);
+            })
+            .catch((e) => {
+                console.error(`An error occurred: ${e}`)
+            });
+    }, []);
+    if (loading) {
 
+        return (<p>Loading...</p>)
+    }
+    return (
+        <>
+            <div className="App">
+                {loading && <p>Loading...</p>}
+                {!loading && <p>Fetched data</p>}
+            </div>
+        </>
+    );
 
 }
