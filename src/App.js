d42c09934a80bde19a50d1a7bcfc7b375b465159
SANFILIPPO Jean-Francois (SNCF VOYAGEURS / ET TGV ATLANTIQUE / Territoire Occitanie) <7407117p@commun.ad.sncf.fr>
2022-12-15 17:09:14 +0100

    navbar => app

--- src/App.js
+++ src/App.js
@@ -10,21 +10,39 @@ import { ListByCountry } from './requests/ListByCountry';
 import { ListingArea } from './requests/ListingArea';
 import { ListingCategory } from "./requests/ListingCategory";
 import { ListingIngredients } from './requests/ListingIngredients';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
+{/*  const [state, setState] = setState(null);*/}
   return (
     <div className='App'>
       <Titre></Titre>
       <BarreDeRecherche></BarreDeRecherche><BoutonRandom></BoutonRandom>
-      <CuisineNavBar></CuisineNavBar>
-      {/*       <AffichageRecette></AffichageRecette> */}
-      {/*       <MealByRandom></MealByRandom> */}
+      <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <NavDropdown title="Search" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">By categories</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">By area</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">By ingredients</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#link">Favoris</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
       {/*       <ListByCountry area="Canadian"></ListByCountry>*/}
       {/*       <AffichageRecette></AffichageRecette> */}
       {/*      <MealByRandom></MealByRandom>*/}
       {/* <ListingArea></ListingArea> */}
       <ListingCategory></ListingCategory>
-{/*      <ListByCountry area="French"></ListByCountry>*/}
+      {/*      <ListByCountry area="French"></ListByCountry>*/}
       {/*<ListingArea></ListingArea>*/}
       {/*      <ListByCountry area="Canadian"></ListByCountry>*/}
       <ListingIngredients></ListingIngredients>
