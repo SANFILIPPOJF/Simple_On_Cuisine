1726b2bf52120b8fbe1c224698ac381964e4a5b1
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2022-12-15 14:39:57 +0100

    S_V2.9 suite 1 ListingArea

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { MealByRandom } from './requests/mealByRandom';
 /* import { AffichageRecette } from './components/affichageRecette'; */
 import { MealByID } from './requests/mealByID';
 import { ListByCountry } from './requests/ListByCountry';
+import { ListingArea } from './requests/ListingArea';
 function App() {
   return (
     <div className='App'>
@@ -17,6 +18,7 @@ function App() {
       <MealByRandom></MealByRandom>
 {/*       <MealByID id="52846"></MealByID>*/}
 {/*      <ListByCountry area="Canadian"></ListByCountry>*/}
+      <ListingArea strArea = "#action/3.2"></ListingArea>
     </div>
   );
 }
--- src/requests/ListingArea.js
+++ src/requests/ListingArea.js
@@ -1,19 +1,21 @@
 import { useEffect, useState } from "react";
 
 
-export function ListingArea (area){
+export function ListingArea (strArea){
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const ListingArea = (areaArray.map(area,meals.strArea) =>(<li key ={meals.strArea}>{meals.strArea} </li>
+    ));
 
     useEffect( ()=> {
-        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`)
+        fetch(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
             .then(response => response.json())
-            .then((area) => {
+            .then((areaArray) => {
                 setLoading(false);
-                console.log(area.strArea);
-                setData(area.strArea);
+                console.log(areaArray);
+                setData(areaArray.meals);
             })
             .catch((e) => {
                 console.error(`An error occurred: ${e}`)
@@ -25,8 +27,6 @@ export function ListingArea (area){
     return (
         <>
         <div className = "App">
-            {loading && <p>Loading...</p>}
-            {!loading && <p>Fetched data</p>}
             </div>
             </>
     );
