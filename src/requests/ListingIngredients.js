53251d5b709bdf1ec611c250e390f159f1887919
PROENCA Sandrine (SNCF VOYAGEURS / ESV TGV Sud Ouest / BCC TOULOUSE) <7410499r@commun.ad.sncf.fr>
2022-12-15 16:06:23 +0100

    S_V2.10 finalisation ListingIngredients

---
+++ src/requests/ListingIngredients.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+
+
+export function ListingIngredients() {
+
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+
+    useEffect(() => {
+        fetch(`https://www.themealdb.com/api/json/v1/1/list.php?i=list`)
+            .then(response => response.json())
+            .then((ingredientsArray) => {
+                setLoading(false);
+                console.log(ingredientsArray);
+                setData(ingredientsArray.meals);
+            })
+            .catch((e) => {
+                console.error(`An error occurred: ${e}`)
+            });
+    }, []);
+    if (loading) {
+        return (<p>Loading...</p>)
+    }
+    console.log(data);
+    const listIngredients = data.map(ingredients => {
+        return(<li key={ingredients.strIngredients}>{ingredients.strIngredients}</li>)
+    })
+    return (
+        <>
+            <ul>
+                {listIngredients}
+            </ul>
+        </>
+    );
+}
