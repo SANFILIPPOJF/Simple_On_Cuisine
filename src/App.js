e1842f736bb8f50fa91f86f76c7065f9dc28babd
Titou <menardjc.33@gmail.com>
2022-12-15 11:30:36 +0100

    feat: affichageRecette.js

--- src/App.js
+++ src/App.js
@@ -4,25 +4,19 @@ import { CuisineNavBar } from "./components/NavBar";
 import { Titre } from './components/Titre';
 import { BoutonRandom } from './components/BoutonRandom';
 import { MealByRandom } from './requests/mealByRandom';
-import { AffichageRecette } from './components/affichageRecette';
-import { ListByCategory } from './requests/ListByCategory';
-/* import { MealByRandom } from './requests/mealByRandom'; */
 /* import { AffichageRecette } from './components/affichageRecette'; */
 import { MealByID } from './requests/mealByID';
 import { ListByCountry } from './requests/ListByCountry';
-import { ListByIngredient } from './requests/ListByIngredient';
 function App() {
   return (
     <div className='App'>
       <Titre></Titre>
       <BarreDeRecherche></BarreDeRecherche><BoutonRandom></BoutonRandom>
       <CuisineNavBar></CuisineNavBar>
-      {/*       <AffichageRecette></AffichageRecette> */}
-      {/*       <MealByRandom></MealByRandom> */}
-      {/*      <ListByCategory></ListByCategory>*/}
-      {/*       <MealByID id="52846"></MealByID>*/}
-      {/*       <ListByCountry area="Canadian"></ListByCountry>*/}
-      {/*<ListByIngredient ingredient="chicken_breast"></ListByIngredient>*/}
+{/*       <AffichageRecette></AffichageRecette> */}
+      <MealByRandom></MealByRandom>
+{/*       <MealByID id="52846"></MealByID>*/}
+{/*      <ListByCountry area="Canadian"></ListByCountry>*/}
     </div>
   );
 }
